eb39e862b02045f161319b6b1e46fdb2303d550a
N3oray <serge.lopes.94@free.fr>
2026-07-01 13:31:08 +0200

    V2.7.6

--- usr/lib/python3/dist-packages/proton_autogen/core.py
+++ usr/lib/python3/dist-packages/proton_autogen/core.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from shutil import which
 import configparser
 
-VERSION = "2.7.5"
+VERSION = "2.7.6"
 
 CONFIG_FILE = os.path.expanduser("~/.config/proton-autogen.conf")
 CONFIG_DIR = os.path.expanduser("~/.config/proton-autogen/games")
