814cc28b13f34dd5c82c956c9faecb751755a16b
N3oray <serge.lopes.94@free.fr>
2026-07-12 14:26:37 +0200

    Clean code

--- usr/lib/python3/dist-packages/proton_autogen/ux/recent_carousel.py
+++ usr/lib/python3/dist-packages/proton_autogen/ux/recent_carousel.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-
 import gi
 import os
 
 gi.require_version("Gtk", "4.0")
 
-from gi.repository import Gtk, Pango
+from gi.repository import Gtk
 
-from proton_autogen.stats import get_game_badges
+from proton_autogen.ux.recent_card import RecentGameCard
 
 
 class RecentCarousel(Gtk.Box):
@@ -18,34 +16,39 @@ class RecentCarousel(Gtk.Box):
         on_edit=None,
         lang="en"
     ):
+
         super().__init__(
             orientation=Gtk.Orientation.VERTICAL,
-            spacing=1
+            spacing=0
         )
 
+
+        self.games = []
+        self.lang = lang
         self.on_launch = on_launch
         self.on_edit = on_edit
-        self.lang = lang
-        self.games = []
 
-        # -----------------------------
-        # HORIZONTAL SCROLL
-        # -----------------------------
 
         self.scroll = Gtk.ScrolledWindow()
-
-        self.scroll.set_propagate_natural_height(False)
-        self.scroll.set_min_content_height(0)
-        self.scroll.set_max_content_height(60)
 
         self.scroll.set_policy(
             Gtk.PolicyType.AUTOMATIC,
             Gtk.PolicyType.NEVER
         )
 
-        self.scroll.set_hexpand(True)
+
+        # IMPORTANT
+        self.scroll.set_min_content_height(
+            48
+        )
+
+        self.scroll.set_max_content_height(
+            48
+        )
+
 
         self.flow = Gtk.FlowBox()
+
 
         self.flow.set_orientation(
             Gtk.Orientation.HORIZONTAL
@@ -55,21 +58,20 @@ class RecentCarousel(Gtk.Box):
             Gtk.SelectionMode.NONE
         )
 
-        self.flow.set_homogeneous(False)
 
-        self.scroll.set_child(self.flow)
-
-        self.append(self.scroll)
-
-        # patch espace hauteur
-        self.flow.set_row_spacing(2)
+        self.flow.set_row_spacing(0)
         self.flow.set_column_spacing(6)
 
 
+        self.scroll.set_child(
+            self.flow
+        )
 
-    # -----------------------------
-    # PUBLIC API
-    # -----------------------------
+        self.append(
+            self.scroll
+        )
+
+
 
     def set_games(self, games):
 
@@ -82,202 +84,29 @@ class RecentCarousel(Gtk.Box):
 
         self.flow.remove_all()
 
+
         for game in self.games:
 
-            card = self._create_card(game)
+            card = RecentGameCard(
+                game,
+                self.lang,
+                self.on_launch,
+                self.on_edit
+            )
+
+
+            child = Gtk.FlowBoxChild()
+
+            child.set_child(
+                card
+            )
+
+
+            child.set_margin_top(0)
+            child.set_margin_bottom(0)
+
 
             self.flow.insert(
-                card,
+                child,
                 -1
             )
-
-
-
-    # -----------------------------
-    # GAME CARD
-    # -----------------------------
-
-    def _create_card(self, game):
-
-        box = Gtk.Box(
-            orientation=Gtk.Orientation.VERTICAL,
-            spacing=0
-        )
-
-        box.set_margin_top(0)
-        box.set_margin_bottom(0)
-
-        box.add_css_class(
-            "recent-card"
-        )
-
-        # TITLE
-        title = Gtk.Label(
-            label=game.get(
-                "name",
-                "Unknown"
-            )
-        )
-
-        title.set_xalign(0)
-
-        title.add_css_class(
-            "title-4"
-        )
-
-        # BADGES
-        badges_box = Gtk.Box(
-            orientation=Gtk.Orientation.HORIZONTAL,
-            spacing=4
-        )
-
-        badges_box.set_margin_top(1)
-        badges_box.set_margin_bottom(1)
-
-
-        badges = get_game_badges(game, self.lang)
-
-        for badge in badges:
-
-            badges_box.append(
-                self._create_badge(
-                    badge
-                )
-            )
-
-
-
-        # INFO
-        proton = os.path.basename(
-            game.get(
-                "proton",
-                ""
-            )
-        )
-
-        if not proton:
-            proton = "default"
-
-
-        info = Gtk.Label(
-            label=f"Proton: {proton}"
-        )
-
-        info.set_xalign(0)
-        info.add_css_class("dim-label1")
-
-
-        # BUTTONS
-        buttons = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=4)
-        btn_launch = Gtk.Button(label="▶")
-        btn_launch.add_css_class("btn-launch")
-        btn_launch.set_size_request(30, 24)
-
-
-        btn_launch.connect(
-            "clicked",
-            lambda _b, g=game:
-                self._launch(g)
-        )
-
-
-        btn_edit = Gtk.Button(
-            label="Edit"
-        )
-        btn_edit.add_css_class("btn-edit")
-        btn_edit.set_size_request(50, 24)
-
-        btn_edit.connect(
-            "clicked",
-            lambda _b, g=game:
-                self._edit(g)
-        )
-
-
-        buttons.append(btn_edit)
-        buttons.append(btn_launch)
-
-
-
-        box.append(title)
-        box.append(badges_box)
-        box.append(info)
-        box.append(buttons)
-
-
-        row = Gtk.FlowBoxChild()
-
-        row.set_margin_top(0)
-        row.set_margin_bottom(0)
-        row.set_margin_start(0)
-        row.set_margin_end(0)
-
-        row.set_child(box)
-
-        row.game_data = game
-
-
-        return row
-
-
-
-    # -----------------------------
-    # BADGES
-    # -----------------------------
-
-    def _create_badge(self, badge):
-
-        label = Gtk.Label(
-            label=badge.get(
-                "label",
-                ""
-            )
-        )
-
-        label.add_css_class(
-            "badge"
-        )
-
-
-        css = badge.get(
-            "css",
-            []
-        )
-
-        if isinstance(css, str):
-            css = [css]
-
-
-        for c in css:
-            label.add_css_class(c)
-
-
-        tooltip = badge.get(
-            "text"
-        )
-
-        if tooltip:
-            label.set_tooltip_text(
-                tooltip
-            )
-
-
-        return label
-
-
-
-    # -----------------------------
-    # CALLBACKS
-    # -----------------------------
-
-    def _launch(self, game):
-
-        if self.on_launch:
-            self.on_launch(game)
-
-
-
-    def _edit(self, game):
-
-        if self.on_edit:
-            self.on_edit(game)
