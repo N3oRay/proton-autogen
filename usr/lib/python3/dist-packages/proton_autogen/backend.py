189d46f87faf2394b79cb881e270ae83ded1ac4e
N3oray <serge.lopes.94@free.fr>
2026-07-24 21:13:24 +0200

    clean import - optimisation code

--- usr/lib/python3/dist-packages/proton_autogen/backend.py
+++ usr/lib/python3/dist-packages/proton_autogen/backend.py
@@ -9,24 +9,41 @@ from pathlib import Path
 from proton_autogen.exceptions import ExecutableNotFoundError, ProtonNotFoundError, GameConfigError, PrefixError
 from shutil import which
 from time import perf_counter
-
+from proton_autogen.config import VERSION
 from proton_autogen.utils.logger import StructuredLogger
 from proton_autogen.progress import Progress
 
 from proton_autogen.loader import save_game_config, load_game_config
 from proton_autogen.editor import add_game, edit_game_ui
-from proton_autogen.core import *
+from proton_autogen.core import (
+    DEBUG,
+    VERBOSE,
+    USER_PROFILE,
+    USER_PROFILE_DATA,
+
+    run_game_proton,
+    run_standard,
+
+    has_mangohud,
+    has_gamemode,
+    has_proton_call,
+    has_wine,
+
+    proton_name,
+    proton_path,
+
+    load_proton_paths,
+)
 from proton_autogen.profiles.init import *
 from proton_autogen.i18n import tr, init_language
 from proton_autogen.stats import * #get_game_badges
 from proton_autogen.pa_log import handle_result, result_to_line
-from proton_autogen.diag import find_all_protons, find_proton
+from proton_autogen.diag import find_all_protons, find_proton, print_diagnostic
 # new files:
 from proton_autogen.dector import resolve_game_features
 from proton_autogen.system import detect_system_info
 from proton_autogen.session import finalize_session, notifications
 from proton_autogen.proton_call import launch_proton_call
-
 
 #-------------------------- Init Log -------------------
 logger = StructuredLogger("proton-autogen.backend")
