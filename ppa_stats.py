dc3b0fcd7be0241d91e6e8736f9d8813791ca54e
N3oray <serge.lopes.94@free.fr>
2026-07-25 02:18:47 +0200

    optimisation scan ppa

--- ppa_stats.py
+++ ppa_stats.py
@@ -1,34 +1,27 @@
 from launchpadlib.launchpad import Launchpad
+from packaging.version import Version
 
+MIN_VERSION = Version("2.9.7")
 
-lp = Launchpad.login_anonymously(
-    "ppa-stats",
-    "production"
-)
+lp = Launchpad.login_anonymously("ppa-stats", "production")
 
-ppa = lp.people["n3oray"].getPPAByName(
-    name="proton-autogen"
-)
+ppa = lp.people["n3oray"].getPPAByName(name="proton-autogen")
 
 versions = {}
 
 for binary in ppa.getPublishedBinaries():
+    version = binary.binary_package_version
 
-    key = (
-        binary.binary_package_name,
-        binary.binary_package_version
-    )
+    if Version(version) < MIN_VERSION:
+        break
 
-    downloads = binary.getDownloadCount()
-
-    versions[key] = versions.get(key, 0) + downloads
-
+    key = (binary.binary_package_name, version)
+    versions[key] = versions.get(key, 0) + binary.getDownloadCount()
 
 total = sum(versions.values())
 
 for (name, version), downloads in versions.items():
     print(f"{name} {version}: {downloads}")
 
-print()
-print(f"Versions analysées : {len(versions)}")
+print(f"\nVersions analysées : {len(versions)}")
 print(f"Téléchargements totaux : {total}")
