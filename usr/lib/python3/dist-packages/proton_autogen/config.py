17986d05695fea6ddaba5ec0f05a868b7aa94a7f
N3oray <serge.lopes.94@free.fr>
2026-08-04 01:11:02 +0200

    V3.2.0

--- usr/lib/python3/dist-packages/proton_autogen/config.py
+++ usr/lib/python3/dist-packages/proton_autogen/config.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 
-VERSION = "3.1.9"
+VERSION = "3.2.0"
 
 CONFIG_FILE = os.path.expanduser("~/.config/proton-autogen.conf")
 CONFIG_DIR = os.path.expanduser("~/.config/proton-autogen/games")
